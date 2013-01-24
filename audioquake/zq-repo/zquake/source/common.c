6207365cdd3e39c8281c0f9a04a4540bf5621562
matatk <matatk@me.com>
2013-01-24 22:41:47 +0000

    Build system tidying; support for easier packaging.
    * Refactored compilation script downloads into a function.
    * Added downloading of shareware and mindgrid data (easier to
    distribute these in the bundle).
    * Updated .app build to include ancillary files.
    * Updated clean script and .gitignore correspondingly.
    * The engine now searches for 10 possible pak files, ignoring breaks in
    the sequence (makes packaging much easier).
    * Scripts are quieter and it is possible to optionally not clean out
    the engine code.

--- audioquake/zq-repo/zquake/source/common.c
+++ audioquake/zq-repo/zquake/source/common.c
@@ -905,6 +905,7 @@ void FS_AddGameDirectory (char *dir)
 	search->next = com_searchpaths;
 	com_searchpaths = search;
 
+#ifndef AGRIP
 //
 // add any pak files in the format pak0.pak pak1.pak, ...
 //
@@ -919,7 +920,19 @@ void FS_AddGameDirectory (char *dir)
 		search->next = com_searchpaths;
 		com_searchpaths = search;		
 	}
-
+#else
+	for (i=0 ; i<10 ; i++)
+	{
+		sprintf (pakfile, "%s/pak%i.pak", dir, i);
+		pak = FS_LoadPackFile (pakfile);
+		if (!pak)
+			continue;
+		search = Hunk_Alloc (sizeof(searchpath_t));
+		search->pack = pak;
+		search->next = com_searchpaths;
+		com_searchpaths = search;		
+	}
+#endif
 }
 
 /*
