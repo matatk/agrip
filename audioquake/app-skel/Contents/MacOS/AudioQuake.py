dbea9c47981984143031f10903019f2dc2fd594d
matatk <matatk@me.com>
2013-02-21 23:36:28 +0000

    Launcher speaks (in parallel :-)) and is now more cleanly structured.

--- audioquake/app-skel/Contents/MacOS/AudioQuake.py
+++ audioquake/app-skel/Contents/MacOS/AudioQuake.py
@@ -1,59 +1,79 @@
 #!/usr/bin/env python
-import sys
-import os
-import subprocess
+import sys, os  # chdir hack
+import subprocess  # controller
 from GUI import Window, Button, application
 
-class SingletonWindow(Window):
+
+class GameController(object):
+	_opts_default = ("./zquake-glsdl", "-window", "+set sensitivity 0")
+	_opts_tutorial = ("+coop 0", "+deathmatch 0", "+map agtut01")
+	
+	def _say(self, line):
+		sys.stdout.write("say: " + line)
+		subprocess.Popen(('/usr/bin/say', line))
+
+	def _launch_core(self, command_line):
+		proc = subprocess.Popen(command_line, stdout=subprocess.PIPE)
+		while True:
+			retcode = proc.poll()
+			line = proc.stdout.readline()
+			self._say(line)
+			if retcode is not None:
+				break
+
+	def launch_default(self):
+		self._launch_core(self._opts_default)
+	
+	def launch_tutorial(self):
+		self._launch_core(self._opts_default + self._opts_tutorial)
+
+
+class LauncherSingletonWindow(Window):
 	def __init__(self, application, *args, **kwargs):
-		super(SingletonWindow, self).__init__(*args, **kwargs)
+		super(LauncherSingletonWindow, self).__init__(*args, **kwargs)
 		self._application = application
+		self._game = GameController()
 
 		self.auto_position = False
 		self.position = (200, 250)
-		self.size = (120, 150)
+		self.size = (140, 135)
 		self.resizable = 0
 		self.zoomable = 0
 
 		self.add(Button(
 			position = (10, 10), 
-			size = (100, 25),
-		    title = "Frag!",
-			action = self.launch_default
+			size = (120, 25),
+		    title = "Play Quake",
+			action = self.btn_default
 		))
 		
 		self.add(Button(
 			position = (10, 40), 
-			size = (100, 25),
-		    title = "Tutorial",
-			action = self.launch_tutorial
+			size = (120, 25),
+		    title = "Play Tutorial",
+			action = self.btn_tutorial
 		))
 
 		self.add(Button(
-			position = (10, 70), 
-			size = (100, 25),
-		    title = "Quit",
+			position = (10, 100), 
+			size = (120, 25),
+		    title = "Quit Launcher",
 			action = self.close_cmd
 		))
 	
 	def close_cmd(self):
 		self._application.quit_cmd()
 	
-	def launch_default(self):
-		subprocess.call(
-			("./zquake-glsdl", "-window", "+set sensitivity 0")
-		)
+	def btn_default(self):
+		self._game.launch_default()
 
-	def launch_tutorial(self):
-		subprocess.call(
-			("./zquake-glsdl", "-window", "+set sensitivity 0",
-				"+coop 0", "+deathmatch 0", "+map agtut01")
-		)
+	def btn_tutorial(self):
+		self._game.launch_tutorial()
 
 
 if __name__ == '__main__':
 	os.chdir(os.path.dirname(sys.argv[0]))
 	app = application()
-	launcher = SingletonWindow(app)
+	launcher = LauncherSingletonWindow(app)
 	launcher.show()
 	app.run()
