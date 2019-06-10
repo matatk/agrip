3949d822eaa72edf955fe6f4549d7d62931a6977
Matthew Tylee Atkinson <matatk@gmail.com>
2019-06-10 08:18:02 +0100

    Be explicit about testing by adding a shortcut test script
    This also adds clarity to the existing compare script.

--- ldl/compare-orig-new-bsp-sizes.sh
+++ ldl/compare-orig-new-bsp-sizes.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# The "*-orig.map" files came from a run of the LDL code before the move to
+# Python 3. This script checks the sizes of BSP files created from those maps
+# and ones freshly converted from XML.
 mismatched_count=0
 for bsp in *.bsp; do
 	orig=${bsp%.*}-orig.bsp
@@ -13,5 +16,9 @@ for bsp in *.bsp; do
 		fi
 	fi
 done
-echo $mismatched_count maps differ in size
+if [ $mismatched_count -eq 0 ]; then
+	echo no maps differ in size -- test successful
+else
+	echo "$mismatched_count map(s) differ in size -- test failed"
+fi
 exit $mismatched_count
