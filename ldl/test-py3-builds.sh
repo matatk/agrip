3aaefdf3ef0ce933c88fcbb0c2fa4a5e70518e3b
Matthew Tylee Atkinson <matatk@gmail.com>
2020-08-16 11:51:03 +0100

    [LDL] Move reference maps to dedicated directory

--- ldl/test-py3-builds.sh
+++ ldl/test-py3-builds.sh
@@ -2,12 +2,16 @@
 # Build both the "*-orig.map" files from the pre-Python 3 version of the LDL
 # code as well as freshly converting and building directly from XML files, then
 # compare the sizes of the resultant BSPs.
-./ldl.py build -- *.xml *-orig.map || exit 42
+./ldl.py build -- \
+	first_20080427_1353.xml \
+	test_05_*.xml tut*.xml \
+	reference-maps/*-orig.map \
+	|| exit 42  # TODO doesn't actually exit on error
 
 # Now check the sizes
 mismatched_count=0
 for bsp in *.bsp; do
-	orig=${bsp%.*}-orig.bsp
+	orig=reference-maps/${bsp%.*}-orig.bsp
 	if [ -r "$orig" ]; then
 		origsize=$(stat -f %z "$orig")
 		bspsize=$(stat -f %z "$bsp")
