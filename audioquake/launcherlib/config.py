ee7283753457c88ce911c8e059804860ffff450e
Matthew Tylee Atkinson <matatk@gmail.com>
2020-08-24 21:33:28 +0100

    Fix config file handling
    * Have an explicit function to init the config stuff, because it writes
      to the current directory, and that is changed during startup.
    * Set the __path__ variable, as apparently that was needed (but in a
      non-pkg module, should not be, so just set it to None).

--- audioquake/launcherlib/config.py
+++ audioquake/launcherlib/config.py
@@ -1,7 +1,7 @@
 """AudioQuake Game Launcher - Configuration service"""
 from configparser import ConfigParser
 
-CONFIG_FILENAME = 'audioquake.ini'
+CONFIG_FILENAME = 'audioquake.ini'  # i.e. in the current directory
 
 INITIAL_CONFIG = {
 	'first_game_run': 'yes'
@@ -9,13 +9,29 @@ INITIAL_CONFIG = {
 
 _config = None
 
+# This isn't a package, but __path__ has to be set or __getattr__ gets called
+__path__ = None
+
+
+def init():
+	"""Read the existing config file, or create a default one
+
+	Due to the default path (above) this will create the file in the current
+	directory, so needs to be called in the right place."""
+	global _config
+	_config = ConfigParser()
+	_config.read(CONFIG_FILENAME)
+	if len(_config) == 1:  # always has a default section
+		_config['launcher'] = INITIAL_CONFIG
+		save()
+
 
 def save():
 	with open(CONFIG_FILENAME, 'w') as configfile:
 		_config.write(configfile)
 
 
-def convert_to_string(value):
+def _convert_to_string(value):
 	if value is True:
 		return 'yes'
 	elif value is False:
@@ -24,7 +40,7 @@ def convert_to_string(value):
 		return str(value)
 
 
-def convert_to_type(string):
+def _convert_to_type(string):
 	if string == 'yes' or string == 'true':
 		return True
 	elif string == 'no' or string == 'false':
@@ -33,23 +49,16 @@ def convert_to_type(string):
 		return string
 
 
-def get_or_set_core(name, value=None):
+def _get_or_set_core(name, value=None):
 	if value is None:
-		return convert_to_type(_config['launcher'][name])
+		return _convert_to_type(_config['launcher'][name])
 	else:
-		_config['launcher'][name] = convert_to_string(value)
+		_config['launcher'][name] = _convert_to_string(value)
 		save()
 
 
 def __getattr__(name):
 	if name in _config['launcher']:
-		return lambda value=None: get_or_set_core(name, value)
+		return lambda value=None: _get_or_set_core(name, value)
 	else:
 		raise AttributeError(name)
-
-
-_config = ConfigParser()
-_config.read(CONFIG_FILENAME)
-if len(_config) == 1:  # always has a default section
-	_config['launcher'] = INITIAL_CONFIG
-	save()
