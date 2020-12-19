219698b31eac8392644ca35f5d8390206d1a6edf
Matthew Tylee Atkinson <matatk@gmail.com>
2020-12-19 14:57:35 +0000

    Have LDL's "play" action call the launcher via CLI

--- lib/ldllib/ldllib/play.py
+++ lib/ldllib/ldllib/play.py
@@ -2,44 +2,26 @@
 
 This is only intended to be used by the command-line LDL tool. The AudioQuake
 launcher uses its own method for starting the game.'''
-from os import getcwd, chdir
-from pathlib import Path
 from platform import system
 import subprocess
 import shutil
 
 from buildlib import Config
 
-# FIXME: Make this call the launcher with a special mode
-# FIXME: sort paths
-# FIXME: use build lib paths? don't see an issue; other parts of ldl use it
-# FIXME: may not need this at all?
-base = Path(__file__).parent.parent.parent.parent
+MAPS_DIR = Config.dir_dist_collated / 'data' / 'id1' / 'maps'
+
 if system() == 'Darwin':
-	engine = './zquake-glsdl'
+	# TODO: Couldn't use dir_aq_exe_internals but it's almost right
+	LAUNCHER = Config.dir_dist_collated / 'AudioQuake.app' / 'Contents' \
+		/ 'MacOS' / 'AudioQuake'
 elif system() == 'Windows':
-	engine = 'zquake-gl.exe'
+	LAUNCHER = Config.dir_dist_collated / 'AudioQuake' / 'AudioQuake.exe'
 else:
 	raise NotImplementedError
 
-maps_dir = Config.dir_dist_collated / 'data' / 'id1' / 'maps'
-
-# FIXME: Make this call the launcher with a special mode
-command_line_basis = [
-	engine,
-	'-window',
-	'-width', '640',
-	'-height', '480',
-	'+deathmatch', '0',
-	'+set', 'cl_confirmquit', '0',
-	'+map']
-
 
 def _run(map_base_name, verbose=False):
-	starting_dir = getcwd()
-	# FIXME: Make this call the launcher with a special mode
-	chdir(Config.dir_aq_exe_internals / 'engines')  # FIXME: synch note with spec?
-	command_line = command_line_basis + [map_base_name]
+	command_line = [LAUNCHER, 'play', 'id1', map_base_name]
 	try:
 		res = subprocess.run(command_line, capture_output=True, check=True)
 		if verbose is True:
@@ -49,10 +31,9 @@ def _run(map_base_name, verbose=False):
 		subprocess.run(command_line)
 		print()
 		print('You may need to build AudioQuake and import the registered data.')
-	chdir(starting_dir)
 
 
 def play(bsp_file, verbose=False):
 	print('Playing', bsp_file)
-	shutil.copy(bsp_file, maps_dir)
+	shutil.copy(bsp_file, MAPS_DIR)
 	_run(bsp_file.with_suffix('').name, verbose)
